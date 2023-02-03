cd509bad94044c32d1de2dbaa17d6b28c902b645
AloMartinP <73949637+AloMartinP@users.noreply.github.com>
2023-02-03 20:57:25 +0200

    monkey and game

--- game.js
+++ game.js
@@ -1,11 +1,44 @@
-const game = new Phaser.Game(800,600, Phaser.AUTO, '', {
-    preload: preload,
-    create: create,
-    update: update
-})
 
-function preload () {}
 
-function create() {}
+var config = {
+    type: Phaser.AUTO,
+    backgroundColor: 0xCCFFFF ,
+    width: '100vh',
+    height: '100vh',
+    physics: {
+        default: 'arcade'
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
 
-function update() {}
+var monkey;
+var keyInputs;
+var game = new Phaser.Game(config);
+
+function preload(){
+    this.load.image('monkey', 'img/monkey.png');
+}
+
+function create(){
+    monkey = this.physics.add.sprite(50, 50, 'monkey');
+    keyInputs = this.input.keyboard.createCursorKeys();
+}
+
+function update(){
+    if(keyInputs.left.isDown){
+        monkey.x = monkey.x - 10;
+    }
+    if(keyInputs.right.isDown){
+        monkey.x = monkey.x + 10;
+    }
+    if(keyInputs.up.isDown){
+        monkey.y = monkey.y - 10;
+    }
+    if(keyInputs.down.isDown){
+        monkey.y = monkey.y + 10;
+    }
+}
